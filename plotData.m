cdc538ef674f4cafdb176d914c7867d389b82701
Juan Ignacio Basilio Flores <juan.basflo.initec@gmail.com>
2015-03-31 21:15:03 -0500

    plotData
    creacion de la funcion para graficar los perceptrones

---
+++ plotData.m
@@ -0,0 +1,16 @@
+function plotData(X,T,marco)
+    
+    N = length(T);
+    grid on
+    hold on
+    for i=1:N
+        if (T(i)==0)
+            plot(X(i,1),X(i,2),'gx','MarkerSize',10,'LineWidth',2);
+        elseif (T(i)==1)
+            plot(X(i,1),X(i,2),'bo','MarkerSize',10,'LineWidth',2);
+        end
+    end
+    axis([min(X(:,1))-marco max(X(:,1))+marco min(X(:,2))-marco max(X(:,2))+marco ])
+    xlabel('x1');
+    ylabel('x2');
+end
